065ddcc3cbbb2cf312e1bc57e18dfdca5ce372ee
David Myska <david.myska@thermofisher.com>
2025-11-11 14:54:20 +0100

    module stuff

--- src/frontend/modules/module_status.gd
+++ src/frontend/modules/module_status.gd
@@ -1,18 +1,33 @@
-extends PanelContainer
+extends FoldableContainer
 
-var m_module : Module
+var m_module : Module = null
+var m_header_label : Label = null
 
-func from_module(module : Module) -> void:
-	m_module = module
-	%ManualDisable.disabled = not m_module.can_be_disabled_manually()
+func _ready() -> void:
+	assert(m_module)
+	var lbl := Label.new()
+	lbl.text = "ON"
+	lbl.modulate = Color.GREEN
+	add_title_bar_control(lbl)
+	m_header_label = lbl
+	
+	if not m_module.can_be_disabled_manually():
+		%DisableBtn.disabled = true
+		%DisableBtn.tooltip_text = "This module cannot be disabled manually"
+	
 	m_module.status_changed.connect(func(status : Module.Status, reason : String):
-		%Running.button_pressed = status == Module.ENABLED
+		if status == Module.Status.RUNNING:
+			m_header_label.text = "ON"
+			m_header_label.modulate = Color.GREEN
+		else:
+			m_header_label.text = "OFF"
+			m_header_label.modulate = Color.RED
 		%Reason.text = reason
 	)
 
-func _on_manual_disable_toggled(toggled_on: bool) -> void:
+func _on_disable_btn_toggled(toggled_on: bool) -> void:
 	m_module.disable_manually(toggled_on)
 
 
-func _on_show_hide_btn_toggled(toggled_on: bool) -> void:
-	%ExpandedStatus.visible = toggled_on
+func _on_restart_btn_pressed() -> void:
+	pass # Replace with function body.
---
+++ src/frontend/modules/module_status_old.gd
@@ -0,0 +1,18 @@
+extends PanelContainer
+
+var m_module : Module
+
+func from_module(module : Module) -> void:
+	m_module = module
+	%ManualDisable.disabled = not m_module.can_be_disabled_manually()
+	m_module.status_changed.connect(func(status : Module.Status, reason : String):
+		%Running.button_pressed = status == Module.ENABLED
+		%Reason.text = reason
+	)
+
+func _on_manual_disable_toggled(toggled_on: bool) -> void:
+	m_module.disable_manually(toggled_on)
+
+
+func _on_show_hide_btn_toggled(toggled_on: bool) -> void:
+	%ExpandedStatus.visible = toggled_on
