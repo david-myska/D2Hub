dd4f8570686c7ec97505f1cfd2bb5737f8b4abe8
David Myska <davidmyska@seznam.cz>
2025-11-23 09:19:18 +0100

    Butcher, cow king almost

--- src/backend/d2/achievements/achievements.cpp
+++ src/backend/d2/achievements/achievements.cpp
@@ -15,10 +15,12 @@
 #include "achievements/dungeons/easy/akarat_let_heal_and_kill.h"
 #include "achievements/dungeons/easy/binding_of_baal_kill_at_most_4_minions.h"
 #include "achievements/dungeons/easy/horazons_sanctum_no_portal_kill_have_10_portals.h"
+#include "achievements/dungeons/easy/horror_under_tristram_kill_with_only_magic_items.h"
 #include "achievements/dungeons/extreme/the_void_kill_inarius_before_lilith.h"
 #include "achievements/dungeons/hard/toraja_max_5_edyrem.h"
 #include "achievements/dungeons/moderate/black_road_no_minion_in_circle.h"
 #include "achievements/dungeons/moderate/heart_of_sin_negative_fire_res.h"
+#include "achievements/dungeons/moderate/bull_prince_rodeo_high_roll_charm.h"
 #include "achievements/dungeons/very_easy/death_projector_high_health.h"
 #include "achievements/dungeons/very_hard/xazax_start_at_15_kill_before_30_pentients.h"
 // Rifts
@@ -51,9 +53,11 @@ namespace D2
         result.emplace(id++, Dungeons::Easy::Akarat::LetHealAndKill::Create());
         result.emplace(id++, Dungeons::Easy::BindingOfBaal::KillAtMostFourMinions::Create());
         result.emplace(id++, Dungeons::Easy::HorazonsSanctum::NoPortalKillHave10Portals::Create());
+        result.emplace(id++, Dungeons::Easy::HorrorUnderTristram::KillWithOnlyMagicItems::Create());
 
-        result.emplace(id++, Dungeons::Moderate::HeartOfSin::NegativeFireRes::Create());
+        result.emplace(id++, Dungeons::Moderate::BullPrinceRodeo::HighRollCharm::Create());
         result.emplace(id++, Dungeons::Moderate::BlackRoad::NoMinionInCircle::Create());
+        result.emplace(id++, Dungeons::Moderate::HeartOfSin::NegativeFireRes::Create());
 
         result.emplace(id++, Dungeons::Hard::Toraja::Max5Edyrem::Create());
 
